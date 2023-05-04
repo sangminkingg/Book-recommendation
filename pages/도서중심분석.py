00def6e05c395eeefd131e97919b277edc77b712
Dokoung <118654142+canIhaveanorder@users.noreply.github.com>
2023-05-04 10:39:34 +0900

    Update 도서중심분석.py

--- pages/도서중심분석.py
+++ pages/도서중심분석.py
@@ -23,6 +23,7 @@ st.write('')
 st.write('')
 
 
+
 js = "window.scrollTo(0, document.getElementById('part-1-age-analysis').offsetTop);"
 
     
@@ -31,14 +32,18 @@ st.markdown("<h3 id='part-1-age-analysis'>✅Part 1. 도서 분석</h3>", unsafe
 st.write('')
 st.write('')
 st.write("""
-- 예를들어 Harry Potter 단어가 포함된 단어를 찾으면 해당 정보를 아래와 같이 찾을 수 있음
-- 같은 책이라도 에디션에 따라 나뉘는 종류가 다르게 경우가 있다
+- 도서별 정보 및 이용횟수와 평균 평점을 볼 수 있다. 
 """)
+# 데이터 불러오기 
+df_book = pd.read_csv('data/books.csv')
+df_book.head(10) 
+
+
 
 st.write("""
-##### ✔ 상위 10위 도서별 평점수
-- Wild Animus 책이 21번으로 가장 많은 평점이 달렸습니다.
-- 그 뒤로 Where the Heart Is (Oprah's Book Club (Paperback)), The Da Vinci Code, The Red Tent (Bestselling Backlist) 등이 다음 평점 수를 기록했다.
+##### ✔ 상위 10위 도서별 이용횟수
+- Rich Shapero 작가의 wildAnimus 가 가장 이용률이 높게 나왔다.  
+- 가장 많이 이용한 도서는 WildAnimus by Rich Shapero , The Davinchi Code by Dan Brown, The Lovely Bones: A Novel, Divine Secrets of the Ya-Ya Sisterhood: A Novel by Alice Sebold 이다. 
 """)
 import streamlit as st
 import pandas as pd
@@ -47,10 +52,10 @@ import seaborn as sns
 
 # 데이터 불러오기
 
-df_book = pd.read_csv('data/popbooks.csv')
-
+df_popbook = pd.read_csv('data/popbooks.csv')
+df_popbook
 fig1, ax = plt.subplots(figsize=(10, 6))
-sns.barplot(x='N_ratings', y='Book-Title', data=df_book,palette='Set1')
+sns.barplot(x='N_ratings', y='Book-Title', data=df_popbook,palette='Set1')
 
 # x축 레이블 90도 회전
 ax.set_xticklabels(ax.get_xticklabels(), rotation=50)
