51bd893a7d545a03baf7e9598ace5bbe5f165952
Dokoung <118654142+canIhaveanorder@users.noreply.github.com>
2023-05-04 14:50:20 +0900

    Update 도서중심분석.py

--- pages/도서중심분석.py
+++ pages/도서중심분석.py
@@ -213,7 +213,7 @@ import seaborn as sns
 df_year = pd.read_csv('data/publisher.csv')
 
 fig1, ax = plt.subplots(figsize=(10, 6))
-sns.barplot(x='Book-Rating', y='Publisher', data =df_year, palette='mute')
+sns.barplot(x='Book-Rating', y='Publisher', data =df_year, palette='muted')
 
 # x축 레이블 90도 회전
 ax.set_xticklabels(ax.get_xticklabels(), rotation=50)
